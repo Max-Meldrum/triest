6ff95644858f19ea277a2022bd05d1493004b82b
Max Meldrum <max@meldrum.se>
2017-11-22 18:25:41 +0100

    Add more logic

--- src/main/scala/Triest.scala
+++ src/main/scala/Triest.scala
@@ -1,4 +1,4 @@
-import Triest.Edge
+import Utils.Edge
 import org.apache.flink.api.common.functions.RichMapFunction
 import org.apache.flink.api.scala.ExecutionEnvironment
 import org.apache.flink.api.scala._
@@ -6,38 +6,46 @@ import org.apache.flink.api.scala._
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
-sealed trait Coin
-case object Heads extends Coin
-case object Tails extends Coin
 
 object Triest extends App {
-  type Edge[A,B] = (A,B)
   val env = ExecutionEnvironment.getExecutionEnvironment
 
+  // Well, in this case a file..
   val stream = env.readTextFile("data/out.moreno_blogs_blogs")
+
+  val edges: DataSet[Edge[Int,Int]] = stream.filter(line => line.nonEmpty && !line.startsWith("%"))
+      .map(_.split(" ") match {case Array(a, b) => (a.toInt, b.toInt)})
+
+  val maxEdges = 100
+  edges.map(new Triest[Int, Int](maxEdges))
+    .print()
+
 }
 
 
 /** Following the implementation of the paper
   *
-  * @param m Max Edges
+  * @param m Max Edges in our Sampling
   */
-class Triest[A,B](m: Int) extends RichMapFunction[Edge[A,B], Int] {
+class Triest[A,B](m: Int) extends RichMapFunction[Edge[A,B], Double] {
   // Our counter
-  private var t = 0
+  private[this] var t = 0
 
-  // S in the
-  private var sample = ArrayBuffer[Edge[A,B]]()
+  private[this] var sample = ArrayBuffer[Edge[A,B]]()
 
 
   // this will handle the logic for each "iteration"
-  override def map(edge: Edge[A,B]) = {
+  override def map(edge: Edge[A,B]): Double = {
     t += 1
     if (sampleEdge(edge, t)) {
       sample.append(edge)
-      // update counter
+      updateCounters(Increment, edge)
     }
-    1
+
+    // return latest estimate..
+    // Estimation in the paper
+    // max = (1, (t(t-1)(t-2))/M(M-1)(M-2)))
+    0.5
   }
 
   private def sampleEdge(edge: Edge[A,B], count: Int): Boolean = {
@@ -46,7 +54,10 @@ class Triest[A,B](m: Int) extends RichMapFunction[Edge[A,B], Int] {
     } else {
       flipBiasedCoin((m/count)) match {
         case Heads => {
-
+          val randomEdge = sample(Random.nextInt(m))
+          sample -= randomEdge
+          sample += edge
+          updateCounters(Decrement, randomEdge)
           true
         }
         case Tails => false
@@ -60,4 +71,14 @@ class Triest[A,B](m: Int) extends RichMapFunction[Edge[A,B], Int] {
       case false => Tails
     }
   }
+
+  private def updateCounters(op: Operation, edge: Edge[A,B]): Unit = {
+    // Fetch neighbours..
+    
+    op match {
+      case Increment =>
+      case Decrement =>
+    }
+  }
+
 }
---
+++ src/main/scala/Utils.scala
@@ -0,0 +1,14 @@
+
+
+sealed trait Coin
+case object Heads extends Coin
+case object Tails extends Coin
+
+sealed trait Operation
+case object Increment extends Operation
+case object Decrement extends Operation
+
+
+object Utils {
+  type Edge[A,B] = (A,B)
+}
